87fe9ed8246c2c4aec60064c4f62d49331fed3e3
pranith7 <pashikantipranith7867@gmail.com>
2022-12-23 00:18:52 +0530

    update

--- Striver/AZsheetstriver/4_Strings/Medium/5_Count Number of Substrings.cpp
+++ Striver/AZsheetstriver/4_Strings/Medium/5_Count Number of Substrings.cpp
@@ -1,1 +1,58 @@
-c
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+//User function template for C++
+
+class Solution
+{
+  public:
+    long long int solve(string &s, int k){
+        int n=s.size();
+    	vector<int> freq(26,0);
+    	int l=0,r=0;
+    	long cnt=0;
+    	long ans=0;
+    	while(l<n){
+    	    if(freq[s[l]-'a']==0){
+    	        cnt++;
+    	    }
+    	    freq[s[l]-'a']--;
+    	    while(cnt>k && r<=l){
+    	        freq[s[r]-'a']++;
+    	        if(freq[s[r]-'a']==0){
+    	            cnt--;
+    	        }
+    	        r++;
+    	    }
+    	    ans+=l-r+1;
+    	    l++;
+    	}
+    	return ans;
+    }
+    long long int substrCount (string s, int k) {
+        return solve(s,k)-solve(s,k-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		string s; cin >> s;
+		int k; cin >> k;
+		Solution ob;
+		cout <<ob.substrCount (s, k) << endl;
+	}
+}
+// } Driver Code Ends
--- Striver/AZsheetstriver/4_Strings/Medium/7_Sum of Beauty of all substring.cpp
+++ Striver/AZsheetstriver/4_Strings/Medium/7_Sum of Beauty of all substring.cpp
@@ -1,1 +1,38 @@
-#
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int beautySum(string s) {
+        // support variables
+        int res = 0, chars[123] = {};
+        multiset<int> ms, tmpMs;
+        // computing the frequencies of all the characters in s
+        for (char c: s) chars[c]++;
+        // populating ms initially
+        for (int i = 'a'; i <= 'z'; i++) if (chars[i]) ms.insert(chars[i]);
+        for (int i = 0, currFreq, lmtI = s.size() - 2, ed = s.size() - 1; i < lmtI; i++) {
+            // removing the character before i, if any
+            if (i) {
+                currFreq = --chars[s[i - 1]];
+                ms.erase(ms.find(currFreq + 1));
+                if (currFreq) ms.insert(currFreq);
+            }
+            tmpMs = ms;
+            for (int j = ed, lmtJ = i + 1; j > lmtJ; j--) {
+                // removing the character before i, if any
+                if (j != ed) {  
+                    currFreq = --chars[s[j + 1]];
+                    ms.erase(ms.find(currFreq + 1));
+                    if (currFreq) ms.insert(currFreq);
+                }
+                // increasing res with the difference of extreme frequencies
+                res += *rbegin(ms) - *begin(ms);
+            }
+            // restoring chars and ms
+            for (int j = ed - 1, lmtJ = i + 1; j > lmtJ; j--) ++chars[s[j + 1]];
+            ms = tmpMs;
+        }
+        return res;
+    }
+};
